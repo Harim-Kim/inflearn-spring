29d941cb9808d2105d5765c6106ac512cbb8a5c5
harimkim <yongkal2@yonsei.ac.kr>
2024-02-27 21:15:45 +0900

    AppConfig - bean 등록

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -3,12 +3,17 @@ package hello.core;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
     public static void main(String[] args){
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class); // name은 method 이름으로! method로 등록되므로.  반환 타입.
+
         Member member = new Member(1L, "harim", Grade.VIP);
         memberService.join(member);
 
